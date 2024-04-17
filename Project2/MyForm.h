6f172a7cfd750535f91fe4c43148637661aab0b1
Nikleroid <Nikleroid@REVISION-PC>
2024-04-17 22:34:25 +0300

    Добавьте файлы проекта.

---
+++ Project2/MyForm.h
@@ -0,0 +1,140 @@
+﻿#pragma once
+#include "fstream"
+namespace Project2 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Сводка для MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::Button^ button1;
+	private: String^ rgb_1;
+	private: String^ rgb_2;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	protected:
+
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		System::ComponentModel::Container^ components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(256, 60);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(44, 16);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"label1";
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(23, 128);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(277, 22);
+			this->textBox2->TabIndex = 2;
+			this->textBox2->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox2_TextChanged);
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(408, 162);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 3;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(23, 203);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(277, 22);
+			this->textBox1->TabIndex = 4;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox1_TextChanged);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(577, 263);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->label1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void textBox2_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+		rgb_1 = safe_cast <String^>(sender);
+	}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		std::ofstream sFile("selectionFile.reg");
+		sFile << "Windows Registry Editor Version 5.00\n";
+		sFile << "[HKEY_CURRENT_USER\\Control Panel\\Colors]\n";
+		//sFile << "\"Hilight\" = \"" + rgb_1 + "\"\n";
+		sFile << "[HKEY_CURRENT_USER\\Control Panel\\Colors]\n";
+		//sFile << "\"HotTrackingColor\" = \"" + rgb_2 + "\"\n";
+
+			
+			
+
+					
+	}
+
+	
+	private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+		rgb_2 = safe_cast <String^>(sender);
+	}
+};
+}
